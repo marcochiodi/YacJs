d2e3bf6862b874f7dd0a4655aa25419a930a4b05
marcochiodi <marcochiodi@live.it>
2016-10-14 14:04:25 +0200

    -

--- js/yac.js
+++ js/yac.js
@@ -1,92 +1,92 @@
 (function() {
     var type, selector, data;
+    var viewportx = 600;
+    var viewporty = 400;
+
     function bar(selector, data) {
-        var wrapper = makeSVG("svg", { x: 0, y: 0, width: "100%", height: "100%", viewBox: "0 0 800 600" });
-        var s = makeSVG("svg", { x: "5%", y: "5%", width: "90%", height: "90%", fill: "#22222", style: "overflow:visible;" });
+        var wrapper = makeSVG("svg", {
+            x: 0,
+            y: 0,
+            width: "100%",
+            height: "100%"
+        });
+        var s = makeSVG("svg", {
+            x: "5%",
+            y: "5%",
+            width: "90%",
+            height: "90%",
+            fill: "#22222",
+            style: "overflow:visible;",
+            viewBox: "0 0 " + viewportx + " " + viewporty
+        });
         var ncol = data.columns.length;
         var colw = parseInt(Math.floor(100 / ncol));
         var max = getMax(data.columns);
         max = roundMax(max);
-        var u = 100 / max;
-        //axis
-        var liney = makeSVG("line", { x1: "0%", x2: "0%", y1: "0%", y2: "100%", stroke: "#888888", "stroke-width": 1 });
-        var linex = makeSVG("line", { x1: "0%", x2: "105%", y1: "100%", y2: "100%", stroke: "#888888", "stroke-width": 1 });
-        s.appendChild(liney);
-        s.appendChild(linex);
-        //unit dotted lines
-        for (var c = 0; c < 11; c++) {
-            var h = c * 10 + "%";
-            var ln = makeSVG("line", {
-                x1: "0%",
-                x2: "105%",
-                y1: h,
-                y2: h,
-                stroke: "#888888",
-                "stroke-width": 0.5,
-                "stroke-dasharray": "5, 5"
-            });
-            var txt = makeSVG("text", {
-                    x: "0%",
-                    y: h,
-                    "alignment-baseline": "middle",
-                    "text-anchor": "end"
-                },
-                ((10 - c) * (max / 100) * 10).toFixed(1) + "&nbsp;"
-            )
-            s.appendChild(ln);
-            s.appendChild(txt);
-        }
-        data.columns.forEach(function(e, i){
-            var px = (100 / ncol * i) + 5;
+
+        s.appendChild(generateAxis(max));
+
+        data.columns.forEach(function(e, i) {
+            var px = (viewportx / ncol * i) + 5;
             var cl = makeSVG("rect", {
-                x: px + 2 + "%",
-                y: (100 - (u * e.value)) + "%",
+                x: px + 2,
+                y: (viewporty - (viewporty / max * e.value)),
                 stroke: e.stroke,
                 'stroke-width': "2",
                 fill: e.fill,
-                width: (100 / ncol - 4) + "%",
-                height: (u * e.value) + "%",
+                width: (viewportx / ncol - 4),
+                height: (viewporty / max * e.value),
                 label: e.label,
                 value: e.value,
                 class: "yac-slice " + e.class
             })
             var lb = makeSVG("text", {
-                x: px + (50 / ncol) + "%",
-                y: "102%",
-                width: ((100 / ncol) - 1) + "%",
+                x: px + (viewportx / 2 / ncol),
+                y: viewporty,
+                width: ((viewportx / ncol) - 1),
                 height: "auto",
                 "text-anchor": "middle",
                 "alignment-baseline": "text-before-edge"
             }, e.label);
-            var lbvalue = makeSVG("text", {
-                x: px + (50 / ncol) + "%",
-                y: 99 - (u * e.value) + "%",
-                width: ((100 / ncol) - 1) + "%",
-                "text-anchor": "middle",
-                "alignment-baseline": "baseline"
-            }, e.value);
+            if (data.numbers) {
+                var lbvalue = makeSVG("text", {
+                    x: px + (viewportx / 2 / ncol),
+                    y: (viewporty * 98 / 100) - (viewporty / max * e.value),
+                    width: ((viewportx / ncol) - 1),
+                    "text-anchor": "middle",
+                    "alignment-baseline": "baseline"
+                }, e.value);
+                s.appendChild(lbvalue);
+            }
+
             cl.addEventListener("click", data.click);
             cl.addEventListener("mouseover", data.mouseover);
             s.appendChild(cl);
             s.appendChild(lb);
-            s.appendChild(lbvalue);
+
         })
         wrapper.appendChild(s);
         document.getElementById(selector).appendChild(wrapper);
     }
+
     function pie(selector, data) {
-        var s = makeSVG("svg", { x: 0, y: 0, width: "100%", height: "100%", fill: "yellow", viewBox: "0 0 600 600" });
-        document.getElementById(selector).appendChild(s);
+        var s = makeSVG("svg", { x: "5%", y: "5%", width: "90%", height: "90%", fill: "#22222", style: "overflow:visible;", viewBox: "0 0 " + viewportx + " " + viewporty });
+        var wrapper = makeSVG("svg", { x: 0, y: 0, width: "100%", height: "100%" });
+        wrapper.appendChild(s);
+        document.getElementById(selector).appendChild(wrapper);
+        var k = viewporty;
+        if (viewporty > viewportx) k = viewportx;
+        k = k / 1.5;
+        var cx = viewportx / 2;
+        var cy = viewporty / 2;
         var ncol = data.columns.length + 2;
-        var tot = 300 * Math.PI;
+        var tot = k * Math.PI;
         var totArr = getTot(data.columns);
         var prev = 0;
-        var k = 300;
-        var legenda = "";
 
         data.columns.forEach(function(e, i) {
             var angle = 2 * Math.PI / totArr * e.value;
-            var d = createSvgArc(300, 300, 150, prev, (prev + angle));
+            var d = createSvgArc(cx, cy, k / 2, prev, (prev + angle));
             var path = makeSVG("path", {
                 d: d,
                 fill: e.fill,
@@ -99,36 +99,46 @@
             path.addEventListener("click", data.click);
             path.addEventListener("mouseover", data.mouseover);
             var xa1, xa2, ya1, ya2;
-            var a1 = getHalfArc(300, 300, 150, prev, (prev + angle));
-            var a2 = getHalfArc(300, 300, 200, prev, (prev + angle));
+            var a1 = getHalfArc(cx, cy, k / 2, prev, (prev + angle));
+            var a2 = getHalfArc(cx, cy, k / 2 + 50, prev, (prev + angle));
             var line = makeSVG("line", { x1: a1.x, y1: a1.y, x2: a2.x, y2: a2.y, stroke: "black" });
             var text = makeSVG("text", { x: a2.x, y: a2.y, fill: "black" }, e.value);
-            s.appendChild(line);
+
             s.appendChild(path);
-            s.appendChild(text);
-            var backbox = text.getBBox();
-            var r = makeSVG("rect", {
-                x: backbox.x - 5,
-                y: backbox.y - 3,
-                width: backbox.width + 10,
-                height: backbox.height + 6,
-                fill: "#eeeeee",
-                stroke: "black"
-            });
-            s.appendChild(r);
-            s.appendChild(text);
+            if (data.numbers) {
+                s.appendChild(text);
+                var backbox = text.getBBox();
+                var r = makeSVG("rect", {
+                    x: backbox.x - 5,
+                    y: backbox.y - 3,
+                    width: backbox.width + 10,
+                    height: backbox.height + 6,
+                    fill: "#eeeeee",
+                    stroke: "black"
+                });
+                s.appendChild(line);
+                s.appendChild(r);
+                s.appendChild(text);
+            }
             prev += angle;
         });
     }
+
     function doughnut(selector, data) {
-        var s = makeSVG("svg", { x: 0, y: 0, width: "100%", height: "100%", fill: "none", viewBox: "0 0 300 300" });
+        var s = makeSVG("svg", { x: "5%", y: "5%", width: "90%", height: "90%", fill: "none", viewBox: "0 0 " + viewportx + " " + viewporty, style: "overflow:visible;" });
+        var wrapper = makeSVG("svg", { x: 0, y: 0, width: "100%", height: "100%" });
+        var k = viewporty;
+        if (viewporty > viewportx) k = viewportx;
+        var cx = viewportx / 2;
+        var cy = viewporty / 2;
+
         var ncol = data.columns.length + 2;
-        var tot = 2 * 100 * Math.PI;
+        var tot = k / 1.5 * Math.PI;
         var totArr = getTot(data.columns);
         var prev = 0;
         var text = makeSVG("text", {
-            x: "150",
-            y: "150",
+            x: cx,
+            y: cy,
             "text-anchor": "middle",
             "alignment-baseline": "middle",
             fill: "black"
@@ -136,10 +146,10 @@
         data.columns.forEach(function(e, i) {
             var v = e.value * tot / totArr;
             var circle = makeSVG("circle", {
-                r: "100",
-                cx: "50%",
-                cy: "50%",
-                "stroke-width": "35",
+                r: k / 3,
+                cx: cx,
+                cy: cy,
+                "stroke-width": k / 6,
                 "stroke-dasharray": v + "  " + tot,
                 "stroke-dashoffset": -prev,
                 stroke: e.fill,
@@ -150,11 +160,11 @@
             });
             circle.addEventListener("click", data.click);
             circle.addEventListener("mouseover", function() {
-                    this.setAttribute("stroke-width", "50");
-                    text.innerHTML  = this.getAttribute("label") + ": " + this.getAttribute("value");
+                    this.setAttribute("stroke-width", k / 5);
+                    text.innerHTML = this.getAttribute("label") + ": " + this.getAttribute("value");
                 }) //data.mouseover);
             circle.addEventListener("mouseout", function() {
-                    this.setAttribute("stroke-width", "35");
+                    this.setAttribute("stroke-width", k / 6);
                     text.innerHTML = "";
                 }) //data.mouseover);
 
@@ -162,45 +172,20 @@
             s.appendChild(circle);
             prev += v;
         })
-        document.getElementById(selector).appendChild(s);
+        wrapper.appendChild(s);
+        document.getElementById(selector).appendChild(wrapper);
         s.appendChild(text);
     }
+
     function line(selector, data) {
-        var s = makeSVG("svg", { x: "5%", y: "5%", width: "90%", height: "90%", fill: "#22222", style: "overflow:visible;" });
-        var wrapper = makeSVG("svg", { x: 0, y: 0, width: "100%", height: "100%", viewBox: "0 0 800 600" });
+        var s = makeSVG("svg", { x: "5%", y: "5%", width: "90%", height: "90%", fill: "#22222", style: "overflow:visible;", viewBox: "0 0 " + viewportx + " " + viewporty });
+        var wrapper = makeSVG("svg", { x: 0, y: 0, width: "100%", height: "100%" });
         var ncol = data.columns.length;
         var colw = parseInt(100 / (ncol - 1));
         var max = getMax(data.columns);
         max = roundMax(max);
         var u = 100 / max;
-        //axis
-        var liney = makeSVG("line", { x1: "0%", x2: "0%", y1: "0%", y2: "100%", stroke: "#888888", "stroke-width": 1 });
-        var linex = makeSVG("line", { x1: "0%", x2: "105%", y1: "100%", y2: "100%", stroke: "#888888", "stroke-width": 1 });
-        s.appendChild(liney);
-        s.appendChild(linex);
-        //unit dotted lines
-        for (var c = 0; c < 11; c++) {
-            var h = c * 10 + "%";
-            var ln = makeSVG("line", {
-                x1: "0%",
-                x2: "105%",
-                y1: h,
-                y2: h,
-                stroke: "#888888",
-                "stroke-width": 0.5,
-                "stroke-dasharray": "5, 5"
-            });
-            var txt = makeSVG("text", {
-                    x: "0%",
-                    y: h,
-                    "alignment-baseline": "middle",
-                    "text-anchor": "end"
-                },
-                parseInt((10 - c) * (max / 100) * 10) + "&nbsp;"
-            )
-            s.appendChild(ln);
-            s.appendChild(txt);
-        }
+        s.appendChild(generateAxis(max));
         var ox = "0%";
         var oy = "100%";
         data.columns.forEach(function(e, i) {
@@ -248,6 +233,75 @@
         wrapper.appendChild(s);
         document.getElementById(selector).appendChild(wrapper);
     }
+
+    function line2(selector, data) {
+        var s = makeSVG("svg", { x: "5%", y: "5%", width: "90%", height: "90%", fill: "#22222", viewBox: "0 0 " + viewportx + " " + viewporty, style: "overflow:visible;" });
+        var wrapper = makeSVG("svg", { x: 0, y: 0, width: "100%", height: "100%" });
+        var ncol = data.labels.length - 1;
+        var max = 600; //getMax(data.columns[0]);
+        max = roundMax(max);
+        var u = 100 / max;
+        s.appendChild(generateAxis(max));
+        data.columns.forEach(function(element, index) {
+            var ox = "0";
+            var oy = viewporty;
+            var points = ox + "," + oy + " ";
+            var color = ((data.fill) ? element.color : "none");
+            element.values.forEach(function(e, i) {
+                ox = (viewportx / ncol * i);
+                oy = (viewporty - (viewporty / max * e));
+                if (data.points) {
+                    var cl = makeSVG("circle", {
+                        cx: ox,
+                        cy: oy,
+                        stroke: element.color,
+                        'stroke-width': 1,
+                        fill: element.color,
+                        r: "5",
+                        //label: data.labels[i],
+                        value: e,
+                    })
+                    s.appendChild(cl);
+                }
+                if (data.numbers) {
+                    var lbvalue = makeSVG("text", {
+                        x: ox,
+                        y: (viewporty * 98 / 100) - (viewporty / max * e),
+                        width: (viewportx / ncol),
+                        "text-anchor": "middle",
+                        "alignment-baseline": "baseline"
+                    }, e);
+                    s.appendChild(lbvalue);
+                };
+
+
+                if (index == 0) {
+                    var lb = makeSVG("text", {
+                        x: ox,
+                        y: viewporty,
+                        "text-anchor": "middle",
+                        "alignment-baseline": "text-before-edge"
+                    }, data.labels[i]);
+                    s.appendChild(lb);
+                }
+
+                points += ox + "," + oy + " ";
+            });
+            points += ox + "," + viewporty;
+            var l = makeSVG("polyline", {
+                stroke: element.color,
+                "stroke-width": 3,
+                fill: color,
+                points: points,
+                class: element.class
+            });
+            s.appendChild(l);
+        })
+
+        wrapper.appendChild(s);
+        document.getElementById(selector).appendChild(wrapper);
+    }
+
     function makeSVG(tag, attrs, value) {
         var el = document.createElementNS('http://www.w3.org/2000/svg', tag);
         for (var k in attrs)
@@ -258,9 +312,41 @@
         return el;
     }
 
+    function generateAxis(max) {
+        var g = makeSVG("g");
+        var liney = makeSVG("line", { x1: "0%", x2: "0%", y1: "0%", y2: "100%", stroke: "#888888", "stroke-width": 1 });
+        var linex = makeSVG("line", { x1: "0%", x2: "105%", y1: "100%", y2: "100%", stroke: "#888888", "stroke-width": 1 });
+        g.appendChild(liney);
+        g.appendChild(linex);
+        //unit dotted lines
+        for (var c = 0; c < 10; c++) {
+            var h = c * 10 + "%";
+            var ln = makeSVG("line", {
+                x1: "0%",
+                x2: "105%",
+                y1: h,
+                y2: h,
+                stroke: "#888888",
+                "stroke-width": 0.5,
+                "stroke-dasharray": "5, 5"
+            });
+            var txt = makeSVG("text", {
+                    x: "0%",
+                    y: h,
+                    "alignment-baseline": "middle",
+                    "text-anchor": "end"
+                },
+                parseInt((10 - c) * (max / 100) * 10) + "&nbsp;"
+            )
+            g.appendChild(ln);
+            g.appendChild(txt);
+        }
+        return g
+    }
+
     function getMax(arr) {
         var max = 0;
-        arr.forEach(function(item, i){
+        arr.forEach(function(item, i) {
             if (item.value > max) max = item.value;
         });
 
@@ -283,9 +369,9 @@
 
     function getTot(arr) {
         var tot = 0;
-        arr.forEach(function(item, i){
+        arr.forEach(function(item, i) {
             tot += item.value;
-        });        
+        });
         return tot;
     }
 
@@ -325,22 +411,23 @@
         type = type;
         selector = selector;
         data = data;
+
         switch (type) {
-                case "bar":
-                    selector = bar(selector, data);
-                    break;
-                case "line":
-                    selector = line(selector, data);
-                    break;
-                case "pie":
-                    selector = pie(selector, data);
-                    break;
-                case "doughnut":
-                    selector = doughnut(selector, data);
-                    break;
-            }
+            case "bar":
+                selector = bar(selector, data);
+                break;
+            case "line":
+                selector = line2(selector, data);
+                break;
+            case "pie":
+                selector = pie(selector, data);
+                break;
+            case "doughnut":
+                selector = doughnut(selector, data);
+                break;
+        }
     }
-    yac.prototype.refresh = function () {
-        yac(type,selector,data);
+    yac.prototype.refresh = function() {
+        yac(type, selector, data);
     }
 }());
